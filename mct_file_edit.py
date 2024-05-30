a99021dfbcd06801396cb5d37e7c62457c3b5512
lizhao_home <lizhao_0911@163.com>
2024-05-30 23:24:05 +0800

    mct_file_edit.py 1、增加修改节点x坐标内容代码 2、完善修改截面特性前5行内容代码

--- mct_file_edit.py
+++ mct_file_edit.py
@@ -16,19 +16,34 @@ sec_pro[2]=[30, 31, 32, 33, 34, 35, 36, 37]
 sec_pro[3]=[40, 41, 42, 43]
 sec_pro[4]=[51, 52, 53]
 
-file_name='SHB40-43纵梁.mct'# 模板文件
-with open(file_name, 'r') as file_template:
-	data=file_template.readlines()
-sec_insert_1='       {AREA}, {ASy}, {ASz}, {Ixx}, {Iyy}, {Izz}\n'.format(AREA=sec_pro[0][0], ASy=sec_pro[0][1],  ASz=sec_pro[0][2], Ixx=sec_pro[0][3], Iyy=sec_pro[0][4], Izz=sec_pro[0][5])
-sec_insert_2='       {Cyp}, {Cym}, {Czp}, {Czm}, {Qyb}, {Qzb}, {PERI_OUT}, {PERI_IN}, {Cy}, {Cz}\n'.format(Cyp=sec_pro[1][0], Czp=sec_pro[1][1],  Czm=sec_pro[1][2], Czp=sec_pro[1][3], Qyb=sec_pro[1][4], Qzb=sec_pro[1][5],PERI_OUT=sec_pro[1][6], PERI_IN=sec_pro[1][7], Cy=sec_pro[1][8], Cz=sec_pro[1][9])
-sec_insert_3='       {Y1}, {Y2}, {Y3}, {Y4}, {Z1}, {Z2}, {Z3}, {Z4}\n'.format(Y1=sec_pro[2][0], Y2=sec_pro[2][1],  Y3=sec_pro[2][2], Y4=sec_pro[2][3], Z1=sec_pro[2][4], Z2=sec_pro[2][5],Z3=sec_pro[2][6], Z4=sec_pro[2][7])
-sec_insert_4='       {HT}, {BT}, {T1}, {T2}\n'.format(HT=sec_pro[3][0], BT=sec_pro[3][1], T1=sec_pro[3][2], T2=sec_pro[3][3])
-sec_insert_5='       YES, {ZZ1}, {ZZ3}, YES, , YES, , YES, , [tw], YES, , YES, , YES,\n'.format(ZZ1=sec_pro[4][0], ZZ2=sec_pro[4][1], tw=sec_pro[4][2])
+node_line=28
+node_num1=82
+node_x=list(range(node_num1))
+node_insert=list(range(node_num1))
+for i in range(node_num1):
+	node_insert[i]='     1, {node_x}, 0, 0\n'.format(node_x=node_x[i])
+	print(node_insert[i])
 
-insert_line_num=425
-data.insert(insert_line_num,sec_insert_1)
-print(type(data[425]))
-print(data[425])
+file_name='SHB40-43.mct'# 模板文件
+with open(file_name, 'r') as file_template:
+	data=file_template.readlines()# 读取文件字符串内容做为原始数据列表
+sec_insert=['', '', '', '', '']# 初始截面特性列表块
+sec_insert[0]='       {AREA}, {ASy}, {ASz}, {Ixx}, {Iyy}, {Izz}\n'.format(AREA=sec_pro[0][0], ASy=sec_pro[0][1],  ASz=sec_pro[0][2], Ixx=sec_pro[0][3], Iyy=sec_pro[0][4], Izz=sec_pro[0][5])
+sec_insert[1]='       {Cyp}, {Cym}, {Czp}, {Czm}, {Qyb}, {Qzb}, {PERI_OUT}, {PERI_IN}, {Cy}, {Cz}\n'.format(Cyp=sec_pro[1][0], Cym=sec_pro[1][1],  Czm=sec_pro[1][2], Czp=sec_pro[1][3], Qyb=sec_pro[1][4], Qzb=sec_pro[1][5],PERI_OUT=sec_pro[1][6], PERI_IN=sec_pro[1][7], Cy=sec_pro[1][8], Cz=sec_pro[1][9])
+sec_insert[2]='       {Y1}, {Y2}, {Y3}, {Y4}, {Z1}, {Z2}, {Z3}, {Z4}\n'.format(Y1=sec_pro[2][0], Y2=sec_pro[2][1],  Y3=sec_pro[2][2], Y4=sec_pro[2][3], Z1=sec_pro[2][4], Z2=sec_pro[2][5],Z3=sec_pro[2][6], Z4=sec_pro[2][7])
+sec_insert[3]='       {HT}, {BT}, {T1}, {T2}\n'.format(HT=sec_pro[3][0], BT=sec_pro[3][1], T1=sec_pro[3][2], T2=sec_pro[3][3])
+sec_insert[4]='       YES, {ZZ1}, {ZZ3}, YES, , YES, , YES, , [tw], YES, , YES, , YES,\n'.format(ZZ1=sec_pro[4][0], ZZ3=sec_pro[4][1], tw=sec_pro[4][2])
+
+ins_start=425# 文件修改起始行
+ins_line_num=5# 文件修改行数
+for i in range(ins_line_num):
+	j=i+ins_start
+	data.insert(j,sec_insert[i])# 逐行插入修改
+	del data[j+1]# 删除被修改的原行
+
+
+
+
 
 project_name='A0-A3.mct'# 模型文件名，即midas软件的.mct文件
 with open(project_name, 'w') as file_object:
